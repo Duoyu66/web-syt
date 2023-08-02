66c5b95d65b104394bf32166911fefe0b95bc513
lbl <2523890936@qq.com>
2023-08-02 22:34:56 +0800

    src路径简化

--- web-syt/vite.config.ts
+++ web-syt/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+//引入node内置的模块path，可以获取绝对路径
+import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve:{
+    alias:{
+      "@":path.resolve(__dirname,'src')
+    }
+  }
 })
