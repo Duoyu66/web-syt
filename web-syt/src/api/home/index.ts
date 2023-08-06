fb2773c66a0b0ac253f2c5eb0cceadc5a2c4eaf8
lbl <2523890936@qq.com>
2023-08-06 14:44:24 +0800

    添加医院等级和地区接口

--- web-syt/src/api/home/index.ts
+++ web-syt/src/api/home/index.ts
@@ -3,7 +3,8 @@ import type {HospitalResponseData} from "@/api/home/types";
 //通过枚举管理首页模块的接口地址
 enum API{
 //    获取已有的医院的数据接口地址
-    HOSPITAL_URL='/api/hosp/hospital'
+    HOSPITAL_URL='/api/hosp/hospital',
+    //获取医院的等级与地区接口
 }
 //获取页面的数据
 export const reqHospital = (page:number,limit:number)=>request.get<any,HospitalResponseData>(API.HOSPITAL_URL+`/${page}/${limit}`)
