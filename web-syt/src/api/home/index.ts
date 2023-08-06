d39d0fb642a5796690f444b64bb526740debf914
lbl <2523890936@qq.com>
2023-08-06 14:47:51 +0800

    添加接口变量

--- web-syt/src/api/home/index.ts
+++ web-syt/src/api/home/index.ts
@@ -5,6 +5,7 @@ enum API{
 //    获取已有的医院的数据接口地址
     HOSPITAL_URL='/api/hosp/hospital',
     //获取医院的等级与地区接口
+    HOSPITALLEVELANDREGION_URL='/cmn/dict/findByDicCode'
 }
 //获取页面的数据
 export const reqHospital = (page:number,limit:number)=>request.get<any,HospitalResponseData>(API.HOSPITAL_URL+`/${page}/${limit}`)
